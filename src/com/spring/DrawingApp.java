7d8b27523ce893fe16b1309e199a28635c019505
thearasengkh <seng.theara.kh@gmail.com>
2017-03-17 16:03:49 +0700

    bean collection

---
+++ src/com/spring/DrawingApp.java
@@ -0,0 +1,20 @@
+package com.spring;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+		
+//		BeanFactory wEfactory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+//		Triangle triangle = (Triangle)factory.getBean("triangle");
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle)context.getBean("triangle");
+		triangle.draw();
+	}
+
+}
